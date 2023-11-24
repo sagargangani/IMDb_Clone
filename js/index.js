75df717efcb7b23821e588139357947b2920b3fb
Sagar Gangani <ganganisagar33@gmail.com>
2023-11-23 19:53:17 -0500

    created basic home page template

---
+++ js/index.js
@@ -0,0 +1,86 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const movie_url =
+    "https://api.themoviedb.org/3/movie/top_rated?api_key=93d7a9f8ea42eb50058f6fc4f4396c4e";
+  const tv_url =
+    "https://api.themoviedb.org/3/tv/top_rated?api_key=93d7a9f8ea42eb50058f6fc4f4396c4e";
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: "Bearer 93d7a9f8ea42eb50058f6fc4f4396c4e",
+    },
+  };
+
+  const movieContainer = document.querySelector(".movie-container");
+  const tvContainer = document.querySelector(".tv-container");
+
+  fetch(movie_url, options)
+    .then((res) => res.json())
+    .then((data) => {
+      const movies = data.results.slice(0, 4);
+
+      movies.forEach((movie) => {
+        const movieCard = createMovieCard(movie);
+        movieContainer.appendChild(movieCard);
+      });
+    })
+    .catch((err) => console.error("error:" + err));
+
+  fetch(tv_url, options)
+    .then((res) => res.json())
+    .then((data) => {
+      const tvShows = data.results.slice(0, 4);
+
+      tvShows.forEach((tv) => {
+        const tvCard = createTVShowCard(tv);
+        tvContainer.appendChild(tvCard);
+      });
+    })
+    .catch((err) => console.error("error:" + err));
+
+  function createMovieCard(movie) {
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movie-card");
+
+    const imagePath = movie.poster_path
+      ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+      : "https://via.placeholder.com/150";
+    const movieInfo = `
+      <div class="card-image">
+        <img src="${imagePath}" alt="${movie.title}" />
+      </div>
+      <div class="card-content">
+        <h2>${movie.title}</h2>
+        <p><strong>Release Date:</strong> ${movie.release_date}</p>
+        <p><strong>Rating:</strong> ${movie.vote_average}</p>
+        <p>${movie.overview}</p>
+      </div>
+    `;
+
+    movieCard.innerHTML = movieInfo;
+    return movieCard;
+  }
+
+  function createTVShowCard(tv) {
+    const tvCard = document.createElement("div");
+    tvCard.classList.add("tv-card");
+
+    const imagePath = tv.poster_path
+      ? `https://image.tmdb.org/t/p/w500${tv.poster_path}`
+      : "https://via.placeholder.com/150";
+    const tvInfo = `
+      <div class="card-image">
+        <img src="${imagePath}" alt="${tv.name}" />
+      </div>
+      <div class="card-content">
+        <h2>${tv.name}</h2>
+        <p><strong>First Air Date:</strong> ${tv.first_air_date}</p>
+        <p><strong>Rating:</strong> ${tv.vote_average}</p>
+        <p>${tv.overview}</p>
+      </div>
+    `;
+
+    tvCard.innerHTML = tvInfo;
+    return tvCard;
+  }
+});
