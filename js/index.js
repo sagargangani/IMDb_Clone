1d7cdd7c22d9286a5834fc008f40aec1c93879d3
Sagar Gangani <ganganisagar33@gmail.com>
2023-11-27 18:17:22 -0500

    carousel added to home page

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
     "https://api.themoviedb.org/3/movie/top_rated?api_key=93d7a9f8ea42eb50058f6fc4f4396c4e";
   const tv_url =
     "https://api.themoviedb.org/3/tv/top_rated?api_key=93d7a9f8ea42eb50058f6fc4f4396c4e";
+
+  const popular =
+    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=93d7a9f8ea42eb50058f6fc4f4396c4e";
   const options = {
     method: "GET",
     headers: {
@@ -89,3 +92,33 @@ function toggleTheme() {
   const body = document.body;
   body.classList.toggle("dark-theme");
 }
+
+async function fetchMovies() {
+  const apiUrl =
+    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=93d7a9f8ea42eb50058f6fc4f4396c4e";
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data.results.slice(0, 100);
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+async function populateCarousel() {
+  const movies = await fetchMovies();
+  const carouselInner = document.querySelector(".carousel-inner");
+
+  movies.forEach((movie, index) => {
+    const isActive = index === 0 ? "active" : "";
+    carouselInner.innerHTML += `
+      <div class="carousel-item ${isActive}">
+        <img class="d-block w-100" src="https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="${movie.title}" />
+      </div>
+    `;
+  });
+}
+
+populateCarousel();
